8bc09722d42ac4761f2938f523fef4b0b28cb5bb
Filippo Romeo <romeofilippo95@gmail.com>
2025-04-16 15:08:33 +0100

    skipgramm integrationx10

--- src/word2vec_dataset.py
+++ src/word2vec_dataset.py
@@ -4,31 +4,40 @@ from torch.utils.data import Dataset
 import numpy as np
 
 class Word2VecDataset(Dataset):
-    def __init__(self, tokens, word_to_ix, ix_to_word, context_size=5):
+    def __init__(self, tokens, word_to_ix, ix_to_word, context_size=5, mode="cbow"):
         """
         Args:
             tokens: List of tokenized words
             word_to_ix: Dictionary mapping words to indices
             ix_to_word: Dictionary mapping indices to words
             context_size: Half the size of the context window
+            mode: "cbow" or "skipgram"
         """
         self.context_size = context_size
         self.tokens = tokens
         self.word_to_ix = word_to_ix
         self.ix_to_word = ix_to_word
-        self.unk_idx = word_to_ix.get('<unk>', 1)
+        self.unk_idx = word_to_ix.get('<UNK>', 0)  # Default to 0 if not found
+        self.mode = mode  # Either 'cbow' or 'skipgram'
         
         # Generate training pairs
         self.data = []
         for i in range(context_size, len(tokens) - context_size):
-            context = (
+            context_indices = (
                 [self.word_to_ix.get(tokens[j], self.unk_idx) 
                  for j in range(i - context_size, i)] +
                 [self.word_to_ix.get(tokens[j], self.unk_idx) 
                  for j in range(i + 1, i + context_size + 1)]
             )
             target = self.word_to_ix.get(tokens[i], self.unk_idx)
-            self.data.append((context, target))
+            
+            if self.mode == "cbow":
+                # CBOW: Input = context, Output = target
+                self.data.append((context_indices, target))
+            elif self.mode == "skipgram":
+                # Skipgram: Input = target, Output = context (multiple context words per target)
+                for context_word in context_indices:
+                    self.data.append((target, context_word))
 
     def __len__(self):
         return len(self.data)
@@ -36,31 +45,3 @@ class Word2VecDataset(Dataset):
     def __getitem__(self, idx):
         context, target = self.data[idx]
         return torch.tensor(context, dtype=torch.long), torch.tensor(target, dtype=torch.long)
-
-class Word2VecDataset:
-    def __init__(self, tokens, word_to_ix, ix_to_word, context_size=5, mode="cbow"):
-        self.tokens = tokens
-        self.word_to_ix = word_to_ix
-        self.ix_to_word = ix_to_word
-        self.context_size = context_size
-        self.mode = mode  # "cbow" or "skipgram"
-
-    def __len__(self):
-        return len(self.tokens) - 2 * self.context_size
-
-    def __getitem__(self, idx):
-        center = idx + self.context_size
-        context_indices = [
-            idx + i for i in range(2 * self.context_size + 1) 
-            if i != self.context_size
-        ]
-        
-        if self.mode == "cbow":
-            context = [self.word_to_ix[self.tokens[i]] for i in context_indices]
-            target = self.word_to_ix[self.tokens[center]]
-            return torch.tensor(context), torch.tensor(target)
-        elif self.mode == "skipgram":
-            target = self.word_to_ix[self.tokens[center]]
-            context = [self.word_to_ix[self.tokens[i]] for i in context_indices]
-            # Return multiple (target, context) pairs per window
-            return torch.tensor(target), torch.tensor(context[0])  # Simplified: use 1 context
