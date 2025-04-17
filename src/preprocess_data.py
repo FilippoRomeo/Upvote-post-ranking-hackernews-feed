148b5c875e365826f360902678a3767afe0a3db8
Filippo Romeo <romeofilippo95@gmail.com>
2025-04-17 20:35:30 +0100

    mismat

--- src/preprocess_data.py
+++ src/preprocess_data.py
@@ -30,11 +30,16 @@ def load_hacker_news_data(database_connection):
     return df
 
 def get_avg_embedding(title_tokens, word2vec_model):
-    embeddings = [word2vec_model[token] for token in title_tokens if token in word2vec_model]
-    if embeddings:
-        return np.mean(embeddings, axis=0)
-    else:
-        return np.zeros(word2vec_model.vector_size)  # Default to zeros if no embeddings
+    embeddings = []
+    for token in title_tokens:
+        try:
+            embeddings.append(word2vec_model.get_embedding(token))
+            except KeyError:
+                continue
+            if embeddings:
+                return np.mean(embeddings, axis=0)
+            else:
+                return np.zeros(word2vec_model.vector_size)  # Default to zeros if no embeddings
 
 def preprocess_data(df, word2vec_model):
     # Tokenize the titles and get their average embeddings
